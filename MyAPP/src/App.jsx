3cbf46165d823c2a4def48d1aab17059f905bdbb
NICK <169161801+ydv129@users.noreply.github.com>
2025-02-05 14:12:44 +0000

    n

---
+++ MyAPP/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Login from './component/Auth/Login.jsx'
+
+function App() {
+  return (
+
+    <div>
+      <Login />
+    </div>
+
+  )
+}
+
+export default App
---
+++ MyAPP/src/component/Auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+        <div className='flex item-center justify-center'>
+        <div className='border-2 border-red-600'>
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+        
+    </div>
+    
+  )
+}
